dfbc55469433b16cc4568e9444d013fc31ff6664
SUBO79 <92024460+SUBO79@users.noreply.github.com>
2023-11-17 22:24:42 +0700

    update giỏ hàng

--- Catalog.php
+++ Catalog.php
@@ -26,37 +26,10 @@
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
-        //   xữ lý add
-        if(isset($_POST['submit'])){
 
-             if(isset($_POST['mat_hang_id'])){
-              if(isset($_SESSION["cart"])){
-                  
-
-
-              }else{
-                $_SESSION["cart"] = array();
-               
-
-              }
-               
-              $id_mat_hang= $_POST['mat_hang_id'];
-              if(!in_array($id_mat_hang,$_SESSION["cart"])){
-                
-                echo "<script>alert('thêm sản phẩm thành công vào giỏ hàng');</script>";
-                array_push($_SESSION["cart"], $id_mat_hang);
-              }
-              
-
-
-
-             }
-
-
-        }
         if(isset($_GET["search"]))
         {
-            $search = $_GET["search"]; 
+            $search = $_GET["search"];
         }
         else
         {
@@ -71,6 +44,7 @@
 
         // Lấy số trang
         $pages = $total_rows / 2;
+        $pages = $pages + $total_rows % 2;
 
         // Lấy trang hiện tại
         $current_page = (isset($_GET['page'])) ? $_GET['page'] : 1;
@@ -82,11 +56,12 @@
         $sql = "SELECT mat_hang.mat_hang_id, mat_hang.mo_ta,mat_hang.ten_mat_hang, mat_hang.don_gia, the_loai.ten_the_loai, dev_team.dev_name, mat_hang.anh
         FROM mat_hang
         JOIN dev_team ON mat_hang.dev_team_id = dev_team.dev_id
-        JOIN the_loai ON mat_hang.the_loai = the_loai.the_loai_id WHERE ten_mat_hang LIKE '%$search%' LIMIT $offset, 5";
+        JOIN the_loai ON mat_hang.the_loai = the_loai.the_loai_id WHERE ten_mat_hang LIKE '%$search%' LIMIT $offset, 2";
         $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
+                echo "<form method='post' action='#'>";
                 echo "<div style='display: flex; width:800px; margin-bottom: 10px;'>";
                 echo "<div style='width: 20%; margin-top: 20px;'>";
                 echo "<img src='game_img/{$row['anh']}' width='100px'>";
@@ -99,17 +74,46 @@
                 echo "<div style='display: flex;width:800px;'>";
                 echo "<div style='width: 50%; margin-left: 160px; color: green;'>".$row['dev_name']."</div>";
                 echo "<div style='width: 25%;text-align: right;margin-right: 10px; color: red;'>".$row['don_gia']."VNĐ"."</div>";
-                echo "<div style='width: 25%; text-align: left;'>";
-                echo "<a href='detail.php?id=" . $row['mat_hang_id'] . "' style='color: green;'>Xem sản phẩm</a>"; echo "</div>"; echo "<form method='post'>
-                <input type='hidden' name='mat_hang_id' value='{$row["mat_hang_id"]}'>
-                <input type='submit' name='submit' value='Thêm vào giỏ'>
-                
-                </form>
-                ";
-                
-                
+                echo "<div style='width: 25%;text-align: left;'>"."<a href='' style='color: green;'> Xem sản phẩm</a>"."</div>";
+                echo "<input type='submit' name='addcart' value='thêm vào giỏ'>";
                 echo "</div>";
                 echo "<br>";
+                echo "</form>";
+                
+                if(isset($_POST['addcart'])) {
+                    $mat_hang_id = $row['mat_hang_id'];
+                    $khach_hang_id = $_SESSION['logged_in']; 
+                    $sql = "SELECT COUNT(*) AS product_count FROM ctdh WHERE mat_hang_id = $mat_hang_id AND don_hang_id IN (SELECT don_hang_id FROM don_hang WHERE khach_hang_id = $khach_hang_id)";
+                    $result_check = $conn->query($sql);
+                    $row_check = $result_check->fetch_assoc();
+
+                    if ($row_check['product_count'] == 0) {
+                        // Product is not in the cart, so add it
+                        $sql = "SELECT * FROM don_hang WHERE khach_hang_id = $khach_hang_id";
+                        $result = $conn->query($sql);
+                        if ($result->num_rows>0) {
+                            $row = $result->fetch_assoc();
+                            $don_hang_id = $row['don_hang_id'];
+                        } else {
+                            $sql = "INSERT INTO don_hang (khach_hang_id) VALUES ('$khach_hang_id')";
+                            if ($conn->query($sql) === TRUE) {
+                                $don_hang_id = $conn->insert_id;
+                                echo "<script>alert('thêm đơn hàng thành công','Thông báo từ hệ thống');</script>";
+                            } else {
+                                echo "<script>alert('lỗi','Thông báo từ hệ thống');</script>" . $conn->error;
+                            }
+                        }
+                        $sql = "INSERT INTO ctdh (don_hang_id, mat_hang_id) VALUES ('$don_hang_id', '$mat_hang_id')";
+                        if ($conn->query($sql) === TRUE) {
+                            echo "<script>alert('thêm đơn hàng thành công','Thông báo từ hệ thống');</script>";
+                        } else {
+                            echo "<script>alert('lỗi','Thông báo từ hệ thống');</script>" . $conn->error;
+                        }
+                    } else {
+                        echo "<script>alert('mặt hàng đã tồn tại','Thông báo từ hệ thống');</script>";
+                    }
+                    
+                }
             }
         }
         echo "<div class='pagination'>";
@@ -119,14 +123,13 @@
             echo "</a>";
         }
         echo "</div>";
-
+        
         $conn->close();
-
-       
     ?>
     </main>
     <div>
     <?php require("footer.php"); ?>
     </div>
+    
 </body>
 </html>
--- cart.php
+++ cart.php
@@ -1,100 +1,90 @@
 <?php
-    
- session_start();
-    $hostname = "localhost";
-    $uname = "root";
-    $passwd = "";
-    $db_name = "Gamers_Alliance";
-
-    $conn = new mysqli($hostname, $uname, $passwd, $db_name);
-//    sữ lý thông tin cung cấp lấy dữ liệu từ database
-    if($conn)
-    {
-        function get_item_info($conn, $item_id)
-        {
-            $info = array();
-            $query = "SELECT * FROM mat_hang WHERE mat_hang_id = $item_id";
-            $result_mh = $conn->query($query);
-            $row_mh = $result_mh->fetch_row();
-            $info["ten_mat_hang"] = $row_mh[1];
-            $info["don_gia"] = $row_mh[2];
-            $info["mo_ta"] = $row_mh[4];
-            $info["anh"] = $row_mh[5];
-            
-            return $info;
-        }
-
-        if(isset($_GET["delete"]) && is_numeric($_GET["delete"]))
-        {
-            $index = $_GET["delete"];
-            unset($_SESSION["cart"][$index]);
-            header("Location: cart.php");
-        }
-    }
+    include("header.php");
 ?>
-<html>
-    <head>
-        <title>My cart</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <style>
-            body {
-                background: linear-gradient(#0c0a0a 25%, #e72c2c 100%) !important;
-                color: white !important
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/footer.css">
+        <link href="https://fonts.cdnfonts.com/css/dec-terminal-modern" rel="stylesheet">
+        <link rel="stylesheet" href="css/index.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <?php
+        // Kết nối database
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "gamers_alliance";
+
+        // Tạo kết nối
+        $conn = mysqli_connect($servername, $username, $password, $database);
+
+        // Kiểm tra kết nối
+        if (!$conn) {
+            die("Kết nối thất bại: " . mysqli_connect_error());
+        }
+        $khach_hang_id = $_SESSION['logged_in'];
+
+        // Lấy danh sách mặt hàng dựa trên mat_hang_id đã xuất hiện trên bảng ctdh
+        $sql = "SELECT
+        dh.don_hang_id,
+        mh.ten_mat_hang,
+        mh.don_gia,
+        mh.the_loai,
+        mh.mo_ta,
+        mh.anh,
+        dt.dev_name,
+        tl.ten_the_loai
+      FROM
+        don_hang AS dh
+      INNER JOIN
+        ctdh AS c ON dh.don_hang_id = c.don_hang_id
+      INNER JOIN
+        mat_hang AS mh ON c.mat_hang_id = mh.mat_hang_id
+      LEFT JOIN
+        dev_team AS dt ON mh.dev_team_id = dt.dev_id
+      LEFT JOIN
+        the_loai AS tl ON mh.the_loai = tl.the_loai_id
+      WHERE
+        dh.khach_hang_id = $khach_hang_id;";
+
+        $result = $conn->query($sql);
+
+        // Kiểm tra kết quả
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo "<form method='post' action='#'>";
+                echo "<div style='display: flex; width:800px; margin-bottom: 10px; margin-left: 300px'>";
+                echo "<div style='width: 20%; margin-top: 20px;'>";
+                echo "<img src='game_img/{$row['anh']}' width='100px'>";
+                echo "</div>";
+                echo "<div style='width: 80%;'>";
+                echo "<div style='text-align: center;'>" . $row['ten_mat_hang'] . "</div><br>";
+                echo substr($row['mo_ta'], 0, 100). "...";
+                echo "</div>";
+                echo "</div>";
+                echo "<div style='display: flex;width:800px; margin-left: 300px'>";
+                echo "<div style='width: 50%; margin-left: 160px; color: green;'>".$row['dev_name']."</div>";
+                echo "<div style='width: 25%;text-align: right;margin-right: 10px; color: red;'>".$row['don_gia']."VNĐ"."</div>";
+                echo "<div style='width: 25%;text-align: left;'>"."<a href='' style='color: green;'> Xem sản phẩm</a>"."</div>";
+                echo "<input type='submit' name='addcart' value='thêm vào giỏ'>";
+                echo "</div>";
+                echo "<br>";
+                echo "</form>";
             }
-        </style>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    </head>
-    <body>
-        <div style="margin-top: 10px;margin-bottom: 10px;">
-        <div style="width: 75%;margin: auto;">
-            <h1>GIỎ BÁN HÀNG CỦA BẠN</h1>
-                <?php
-                    $tong_tien = 0;
-                    $i = 0 ;
-                    foreach($_SESSION["cart"]  as $phantu)
-                    { 
-                        $info = get_item_info($conn,$phantu);
-                        $img = $info["anh"];
-                        $ten_mh = $info["ten_mat_hang"];
-                        $mo_ta = $info["mo_ta"];
-                        $don_gia = $info["don_gia"];
-                        $tong_tien += $don_gia;
-                        echo 
-                        "
-                            <div style='width: 50%;height:auto;min-height: 50px;margin: auto;box-shadow: 2px 1px 2px 1px;'>
-                                <div style='width: 100%'>
-                                    <table>
-                                        <tr>
-                                            <td>
-                                                <img style='max-width: 100px;max-height: 100px' src='$img'>
-                                            </td>
-                                            <td>
-                                                <h3>$ten_mh</h3>
-                                                <div style='width: 95%;max-height: 100px;overflow-y:scroll;margin: auto;border-style: dashed;border-color: gray;border-width: 1px'>
-                                                    $mo_ta
-                                                </div>
-                                                <div style='float:right;padding: 5px;'>
-                                                    Đơn giá: $don_gia đ <a class='btn btn-danger' href='?delete=$i'>Xóa khỏi giỏ</a>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    </table>
-                                </div>
-                            </div>
-                        ";
-                     $i++;
-                    }
-                    if(count($_SESSION["cart"]) == 0)
-                    {
-                        echo "Không có gì trong giỏ!";
-                    }
-                ?>
-                <div style="float:right;position: relative;padding: 5px;font-size: 16px;">
-                    Tổng tiền: <?php echo $tong_tien; ?>đ <input type="submit" name="submit" class="btn btn-success" style="font-size: 16px; !important" value="Thanh toán">
-                </div>
-            </div>
-        </div>
-        </div>
-     <a href="Catalog.php">Quay lại</a> 
-    </body>
+        } else {
+            echo "Không có mặt hàng nào";
+        }
+
+        // Ngắt kết nối
+        mysqli_close($conn);
+    ?>
+    <?php require("footer.php"); ?>
+</body>
 </html>
